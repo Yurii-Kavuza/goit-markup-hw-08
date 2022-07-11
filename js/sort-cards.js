b0b9fde28ea27701af4a3a61e78a382e6e7a1bac
Yurii Kavuza <kavuzaiurii@gmail.com>
2022-07-12 00:58:32 +0300

    add filter in portfolio

--- js/sort-cards.js
+++ js/sort-cards.js
@@ -1,4 +1,8 @@
 const tagsContainer = document.querySelector('.categories.list');
+const cardContainer = document.querySelector('.our-works__list');
+const cards = [...cardContainer.children];
+
+let currentTagValue;
 
 console.log(tagsContainer);
 
@@ -15,8 +19,32 @@ function onCategoriesClick(e) {
     //     currentActiveBtn.classList.remove("categories__btn--active");
     // }
 
-    currentActiveBtn?.classList.remove("categories__btn--active");
+    currentActiveBtn?.classList.remove("categories__btn--active");  
 
     const nextActiveBtn = e.target;
+    currentTagValue = nextActiveBtn.dataset.value;    
     nextActiveBtn.classList.add('categories__btn--active');
+    
+    if (currentTagValue === "all") {
+        renderMarkup(cards);
+        return;
+    }
+   
+    const sortedEls = showElements(cards, currentTagValue);
+    renderMarkup(sortedEls);
+    
+    
+}
+
+function showElements(elements, value) {
+    if (elements.length === 0) {
+        return;
+    }
+
+    return elements.filter(element => element.dataset.value === value);
+}
+
+function renderMarkup(data) {
+    cardContainer.innerHTML = "";
+    cardContainer.append(...data);
 }
